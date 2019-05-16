779d6b905d0d55d31d5f4d87a37b127b9c799393
Ryan <ryan.poh@gmail.com>
2019-05-16 08:58:32 +0000

    employee show error

--- app.js
+++ app.js
@@ -146,5 +146,5 @@ function isLoggedIn(req,res, next){
 
 
 app.listen(process.env.PORT || 5000, function(){
-	console.log('Skyline v8 server listening on port 3000');
+	console.log('Skyline v8 server listening on port 5000');
 });
