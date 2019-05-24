005817e379a09fc30c81e2c09318b1c9726778c0
Ryan <ryan.poh@gmail.com>
2019-05-25 05:16:32 +0800

    fix process show error

--- routes/api.js
+++ routes/api.js
@@ -7,25 +7,25 @@ var	Process = require('../models/process');
 router.post('/api/update', function(req,res){  //respond as POST eventhough same route. USING REST API
 
 
-//ARDUINO
-	// var employeeID = req.body.employeeID;
-	// var lotId = req.body.lotId;
-	// var lotSize = req.body.lotSize;
-	// var lotQuantity = req.body.lotQuantity;
-	// var jobSheetNumber = req.body.jobSheetNumber;
-	// var processId = req.body.processId; 
-	// var timeTaken = req.body.timeTaken;
-	// var date = req.body.date;
+ARDUINO
+	var employeeID = req.body.employeeID;
+	var lotId = req.body.lotId;
+	var lotSize = req.body.lotSize;
+	var lotQuantity = req.body.lotQuantity;
+	var jobSheetNumber = req.body.jobSheetNumber;
+	var processId = req.body.processId; 
+	var timeTaken = req.body.timeTaken;
+	var date = req.body.date;
 
-//POSTMAN
-	var employeeID = req.query.employeeID;
-	var lotId = req.query.lotId;
-	var lotSize = req.query.lotSize;
-	var lotQuantity = req.query.lotQuantity;
-	var jobSheetNumber = req.query.jobSheetNumber;
-	var processId = req.query.processId; 
-	var timeTaken = req.query.timeTaken;
-	var date = req.query.date;
+// //POSTMAN
+// 	var employeeID = req.query.employeeID;
+// 	var lotId = req.query.lotId;
+// 	var lotSize = req.query.lotSize;
+// 	var lotQuantity = req.query.lotQuantity;
+// 	var jobSheetNumber = req.query.jobSheetNumber;
+// 	var processId = req.query.processId; 
+// 	var timeTaken = req.query.timeTaken;
+// 	var date = req.query.date;
 
 	var detectProcess = processId - lotId;
 
