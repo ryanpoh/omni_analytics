95aaf63929b19de6f71f724bc8c34f2fb2abdcd1
Ryan Poh <ryan.poh@gmail.com>
2019-09-10 23:35:28 +0800

    live data for machines and also post working (MongoDB)

--- public/js/machine_chart.js
+++ public/js/machine_chart.js
@@ -1,5 +1,31 @@
-var months = ['Feb', 'March', 'April', 'May'],
-  averagePerformance = [80, 75, 89, 99];
+// window.onload = function() {
+
+// };
+
+
+// const form = document.getElementById('volt-form');
+
+// form.addEventListener('submit', e => {
+//   //const voltSync = document.querySelector('input[name=optradio]:checked').value;
+//   var voltSync = document.getElementById('1').value;
+//   const data = { volt: voltSync };
+
+//   fetch('http://localhost:3000/machines', {
+//     method: 'post',
+//     body: JSON.stringify(data),
+//     headers: new Headers({
+//       'Content-Type': 'application/json'
+//     })
+//   })
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+//   e.preventDefault();
+// });
+
+var months = ['Feb', 'March', 'April', 'May'];
+var averagePerformance = [80, 75, 89, 99];
 
 machineData = {
   type: 'line',
@@ -31,9 +57,29 @@ machineData = {
   }
 };
 
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach(dataset => {
+    dataset.data.push(data);
+  });
+  chart.update();
+}
+
 var ctx = document.getElementById('progressChart').getContext('2d');
 //new Chart(ctx).Line(machineData);
 
-let progressChart = new Chart(ctx,
-  machineData
-);
+let progressChart = new Chart(ctx, machineData);
+
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('5ed5a1f25586a0760edb', {
+  cluster: 'ap1',
+  forceTLS: true
+});
+
+var channel = pusher.subscribe('machine-1-channel');
+channel.bind('machine-1-sync', function(data) {
+  addData(progressChart, 'June', data.volt);
+  alert(JSON.stringify(data));
+});
--- routes/machines.js
+++ routes/machines.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-//const Pusher = require('pusher');
+const Pusher = require('pusher');
+
+var channels_client = new Pusher({
+  appId: '859243',
+  key: '5ed5a1f25586a0760edb',
+  secret: '513c1a9c26df1f2705eb',
+  cluster: 'ap1',
+  encrypted: true
+});
 
 router.get('/machines', function(req, res) {
   res.render('machine_show');
+});
+
+router.post('/machines', function(req, res) {
+  var volt = req.body.volt;
+
+  dataSync = {
+    volt: volt,
+    active: 1,
+    progress: 70
+  };
+  console.log(dataSync)
+
+  channels_client.trigger('machine-1-channel', 'machine-1-sync', dataSync);
+
+  return res.json({ success: true, message: 'Machine 1 Synced.' });
 });
 
 module.exports = router;
